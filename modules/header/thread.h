213e82ebbe992279b104b58fba3dca71737aaf87
bhargav82 <bhargav0831@gmail.com>
2026-04-30 17:04:12 -0500

    added launching a thread

--- modules/header/thread.h
+++ modules/header/thread.h
@@ -1,7 +1,9 @@
+#include <stdio.h>
+#include <chrono>
+#include <atomic>
+#include <thread>
 #include <pthread.h>
 #include <mach/mach.h>
-#include <stdio.h>
-#include <stdexcept>
 
 enum class QoS {
     QOS_CLASS_DEFAULT = 0,// normal priority
@@ -11,11 +13,49 @@ enum class QoS {
 
 };
 
+
+
+template <typename Func, typename... Args>
+auto launch_thread(Func&& f, Args&& ...args, int group_id, int qos) noexcept {
+    std::atomic<bool> failed(false);
+    std::atomic<bool> running(false);
+
+    auto thread_work = [&]() {
+        if (qos < 0 || qos > 3 || !set_thread_affinity(group_id, qos)) {
+            std::cerr << "Failed to set thread affinity for " << pthread_self() << std::endl;
+            failed = true;
+            return;
+        }
+
+        std::cout << pthread_self() << " has been 'pinned' to group " << group_id << " with QoS " << qos << std::endl;
+        runnning = true;
+        std::forward<Func>(f)((std::forward<Args>(args))...);
+    }
+    
+    std::thread* thread = new std::thread(thread_work);
+
+    // Thread has been scheduled but hasn't started doing any of its work yet
+    if (!failed.load() && !running.load()) {
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+
+    if (failed) {
+        thread->join();
+        delete thread;
+        thread = nullptr
+    }
+    
+    return thread;
+}
+
+
+
+
 /// @brief "Pin" the current thread to a core, Apple doesn't allow this directly but can hint to the OS to schedule threads on given core or shared cache
 /// @param group_id group threads that should be on same core/share cache, but don't contend for same data
 /// @param qos_class enumerated priority level, choose the priority level based on what function thread is executing
 /// @return true if pinning the core was successful, false otherwise
-void set_thread_affinity(int group_id, int qos_class) {
+bool set_thread_affinity(int group_id, int qos_class) {
     // Use thread_policy_set with THREAD_AFFINITY_POLICY to suggest threads with same affinity tag to be scheduled on same core or share L2 cache
 
     // 1. Convert pthread_t to a thread_port_t
@@ -24,17 +64,16 @@ void set_thread_affinity(int group_id, int qos_class) {
     // 2. Set group id and qos class
     thread_affinity_policy policy;
     policy.affinity_tag = group_id;
-    if (qos_class < 0 || qos_class > 3) {
-        qos_class = 0;
-    }
     QoS q = static_cast<QoS>(qos_class);
     if (pthread_set_qos_class_self_np(static_cast<qos_class_t>(q), 0) < 0) {
-        throw std::runtime_error("thread.h: Could not set QoS class.");
+        return false;
     }
     
     // 3. Set thread policy
     kern_return_t k_ret = thread_policy_set(mach_thread, THREAD_STANDARD_POLICY, thread_policy_t(&policy), THREAD_STANDARD_POLICY_COUNT);
     if (k_ret != KERN_SUCCESS) {
-        throw std::runtime_error("thread.h: Could not set thread policy.");
+        return false;
     }
+
+    return true;
 }
