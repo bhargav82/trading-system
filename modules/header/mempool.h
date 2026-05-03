ff187912fa9e4689bc9c878b45098de33cca1038
bhargav82 <bhargav0831@gmail.com>
2026-05-03 18:12:08 -0500

    added insert function for mem pool. tested and seems to be working

--- modules/header/mempool.h
+++ modules/header/mempool.h
@@ -1,19 +1,21 @@
+#pragma once
 #include <vector>
 #include <stdlib.h>
-
+#include <iostream>
+#include <cassert>
 
 
 // Mem pool on stack, used when objects are small or there are very few (won't stack overflow)
-template <typename T, size_t N>
-class MemoryPoolStack {
-    T buffer[N];
-};
+// template <typename T, size_t N>
+// class MemoryPoolStack {
+//     T buffer[N];
+// };
 
 
 template <typename T>
 class MemoryPoolHeap final {
 private:
-    std::byte* buffer;
+    T* buffer;
     std::vector<bool> is_free_list;
     size_t next_free;
     size_t sz;
@@ -29,13 +31,15 @@ private:
                 std::cerr << "Ran out of memory in pool" << std::endl;
                 std::abort();
             }
+            
         }
     };
 
 public:
     explicit MemoryPoolHeap(size_t n) : next_free(0), sz(n) {
-        buffer = new std::byte[n * sizeof(T)];
-        memset(buffer, 0, n * sizeof(T));
+        std::byte* temp = new std::byte[n * sizeof(T)];
+        buffer = reinterpret_cast<T*>(temp);
+        memset(buffer, 0, n);
         is_free_list.resize(n, true);
         
     };
@@ -51,23 +55,29 @@ public:
     // allocate space and update next available index 
     // use placement new (ptr) T()
     template <typename ...Args>
-    T* construct(Args&& ...args) noexcept {
+    [[nodiscard]] T* construct(Args&& ...args) noexcept {
         // 1. Find the address of the next free location
+        T* insert_loc = buffer + next_free;
         // 2. construct the T object usign the forwarded args, use placement new to construct the object in place found above
+        T* t_ = new (insert_loc) T(std::forward<Args>(args)...);
+        // std::cout << "inserted at " << next_free << " ";
         // 3. update the bool at this address to be not free
+        is_free_list[next_free] = false;
         // 4. update the next free free variable
+        update_next_free();
         // 5. return t object
+        return t_;
     }
 
     void destruct(const T* t_) noexcept {
         // 1. Find the index, by subtracting pointer from state (std::byte so need to device by sizeof(T))
-        size_t t_index = (t_ - buffer)/sizeof(T);
-
+        size_t t_index = t_ - buffer;
+        // std::cout << "destroyed at " << t_index << "\n";
         // 2. Assert index is valid
-        ASSERT(t_index >= 0 && t_index < sz && "t_index is out of range");
+        assert(t_index >= 0 && t_index < sz && "t_index is out of range");
 
         // 3. just need to update is_free of that index and destroy object in that space
-        ASSERT(is_free_list[t_index] == false && "t_index is not currently used")
+        assert(is_free_list[t_index] == false && "t_index is not currently used");
         (buffer + t_index * sizeof(T))->~T();
         is_free_list[t_index] = true;
     }
@@ -77,7 +87,7 @@ public:
         // go through all the objects in free list that = false (meaning there is an object here and call destructor)
         for (size_t i = 0; i < sz; ++i) {
             if (!is_free_list[i]) {
-                (buffer + i * sizeof(T))->~T();
+                (buffer + i)->~T();
             }
         }
 
