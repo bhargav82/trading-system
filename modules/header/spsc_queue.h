ac79b5061407a2d8be31c1713d5e0818f8e398f9
bhargav82 <bhargav0831@gmail.com>
2026-05-07 13:28:13 -0400

    started implementing spsc lock free queue

--- modules/header/spsc_queue.h
+++ modules/header/spsc_queue.h
@@ -2,7 +2,6 @@
 #include <mutex>
 
 // Normal thread-safe queue using locks
-// No condition variable -> 
 template <typename T>
 class LockedQueue {
 private:
@@ -28,4 +27,92 @@ public:
         queue.at(0);
         m.unlock();
     };
+};
+
+
+
+// Implement a SPSC circular ring buffer (array), allowing pop and push using indices (no locks)
+
+template <typename T>
+class SPSCQueue {
+public:
+    explicit SPSCQueue(size_t cap) : buffer(new T[cap]), push_ptr(0), pop_ptr(0), capacity(cap), sz(0) {
+       
+    };
+
+    SPSCQueue() = delete;
+    SPSCQueue(const SPSCQueue& other) = delete;
+    SPSCQueue(SPSCQueue&& other) = delete;
+    SPSCQueue& operator=(const SPSCQueue& other) = delete;
+    SPSCQueue& operator=(SPSCQueue&& other) = delete;
+
+    
+    template <Args ...args>
+    void emplace(Args&& ...args) {
+        // inplace construction of object
+    }
+
+    inline [[nodiscard]] bool try_emplace() { 
+        // check if object can be placed in (not full)
+        return !(is_full);
+    }
+    
+
+    template<tpyename ...Args>
+    void push(Args&& ...args) {
+        // try to push the object into the end of the queue
+        // check if the queue is full first
+        // no inplace construction, build object, then push using forwarding 
+    }
+
+    inline [[nodiscard]] bool try_push() {
+        return !(is_full);
+    }
+
+    T* front() {
+        // return the front of the queue if not empty
+        if (!is_empty()) {
+            return buffer[pop_ptr];
+        }
+
+        return nullptr;
+    }
+
+    T& pop() {
+        T* top = front();
+
+        //TODO: check if this will destroy the obhect
+        if (!top) {
+            buffer[pop_ptr++]->T~();
+        }
+
+        return *top; 
+    }
+
+
+    inline bool is_full() {
+        return sz == capacity;
+    }
+
+    inline bool is_empty() {
+        return push_ptr - pop_ptr == 0;
+    }
+
+    SPSCQueue~() {
+        for (size_t i = 0; i < sz; ++i) {
+            if (!std::is_triviailly_destructible(cap[i])) {
+                buffer[i]->T~();
+            }
+        }
+
+        delete[] buffer;
+    }
+
+
+private:
+    T* buffer;
+    size_t push_ptr;
+    size_t pop_ptr;
+    size_t capacity;
+    size_t sz;
 };
